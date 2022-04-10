a8e1b11c22773f4571a875b61b312950c7ae5ac3
karthik-hub111 <63627542+karthik-hub111@users.noreply.github.com>
2022-04-10 12:19:01 +0530

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,28 +1,11 @@
-<!-- social Section Starts Here -->
-<section class="social">
-        <div class="container text-center">
-            <ul>
-                <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
-                </li>
-                <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
-                </li>
-                <li>
-                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
-                </li>
-            </ul>
-        </div>
-    </section>
-    <!-- social Section Ends Here -->
 
-    <!-- footer Section Starts Here -->
-    <section class="footer">
-        <div class="container text-center">
-            <p>All rights reserved. Designed By <a href="#">Dupati Karthik</a></p>
+           <!-- footer section starts -->
+           
+           <div class="footer">
+            <div class="wrapper">
+              <p class="text-center">2022 All rights reserved. Developed By - <a href="#">Dupati Karthik</a></p>
+            </div>
         </div>
-    </section>
-    <!-- footer Section Ends Here -->
-
-</body>
+            <!-- footer section ends -->
+    </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,46 +1,33 @@
-<?php include('config/constants.php'); ?>
+<?php
+ include('../config/constants.php');
+ include('login-check.php');
+
+?>
 
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <!-- Important to make website responsive -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurant Website</title>
 
-    <!-- Link our CSS file -->
-    <link rel="stylesheet" href="css/style.css">
-</head>
+<html>
+    <head>
+        <title>
+            Food ordering website - home page
+        </title>
 
-<body>
-    <!-- Navbar Section Starts Here -->
-    <section class="navbar">
-        <div class="container">
-            <div class="logo">
-                <a href="#" title="Logo">
-                    <img src="images/logo.png" alt="Restaurant Logo" class="img-responsive">
-                </a>
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+    <body>
+        <!-- menu section starts -->
+        <div class="menu">
+            <div class="wrapper">
+               <ul>
+                   <li><a href="index.php">Home</a></li>
+                   <li><a href="manage-admin.php">Admin</a></li>
+                   <li><a href="manage-category.php">Category</a></li>
+                   <li><a href="manage-food.php">Food</a></li>
+                   <li><a href="manage-order.php">Order</a></li>
+                   <li><a href="logout.php">Logout</a></li>
+               </ul>
             </div>
-
-            <div class="menu text-right">
-                <ul>
-                    <li>
-                        <a href="<?php echo SITEURL; ?>">Home</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo SITEURL; ?>categories.php">Categories</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo SITEURL; ?>foods.php">Foods</a>
-                    </li>
-                    <li>
-                        <a href="#">Contact</a>
-                    </li>
-                </ul>
-            </div>
-
-            <div class="clearfix"></div>
+            
         </div>
-    </section>
-    <!-- Navbar Section Ends Here -->
+         <!-- menu section ends -->
